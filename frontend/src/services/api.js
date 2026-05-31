479e8035ff64195eb55d6ab2d01cddddab80d908
urimdibrani-design <urimdibrani@gmail.com>
2026-05-31 23:23:52 +0200

    added restoreSession

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -221,15 +221,6 @@ const requestSessionRefresh = async () => {
   return refreshPromise;
 };
 
-export const restoreSession = async () => {
-  try {
-    return await requestSessionRefresh();
-  } catch {
-    clearStoredAuth({ silent: true });
-    return null;
-  }
-};
-
 export const logoutSession = async () => {
   try {
     await sessionClient.post("/auth/logout");
